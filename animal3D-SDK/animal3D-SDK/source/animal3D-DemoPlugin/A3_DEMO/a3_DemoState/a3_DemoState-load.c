48136ce367122f250e9261def66588acb2edfddb
matthew.esslie <matthew.esslie@mymail.champlain.edu>
2021-09-09 22:32:33 -0400

    giving the error fixes another try

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoState/a3_DemoState-load.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoState/a3_DemoState-load.c
@@ -762,12 +762,12 @@ void a3demo_loadAnimations(a3_DemoState* demoState)
 	a3ui32 i;
 
 	// instantiate and set up a pool of keyframes (20+)
-	a3_KeyframePool* keyframePool;
+	a3_KeyframePool keyframePool;
 	a3keyframePoolCreate(&keyframePool, 20);
 
 	// instantiate and set up clips (5+)
 
-	a3_ClipPool* clipPool0;
+	a3_ClipPool clipPool0;
 	a3clipPoolCreate(&clipPool0, 5);
 
 	// instantiate and set up clip controllers (3+)
@@ -775,7 +775,7 @@ void a3demo_loadAnimations(a3_DemoState* demoState)
 		i < demoStateMaxCount_clipController;
 		++i, ++cc)
 	{
-		a3clipControllerInit(cc, "ClipController" + i, clipPool0, 1);
+		a3clipControllerInit(cc, "ClipController" + i, &clipPool0, 1);
 	}
 	/*
 	a3_ClipController* clipController0;
