12a55b90263b50a90434aa0c743e9f72736f0458
Benjamin Davies <benjamin.davies@mymail.champlain.edu>
2021-09-23 00:11:52 -0400

    Fix for incorrect const assignment within Spatial Pose controller.

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/a3_SpatialPose.h
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/a3_SpatialPose.h
@@ -125,7 +125,7 @@ a3i32 a3spatialPoseReset(a3_SpatialPose* spatialPose);
 a3i32 a3spatialPoseConvert(a3mat4* mat_out, const a3_SpatialPose* spatialPose_in, const a3_SpatialPoseChannel channel, const a3_SpatialPoseEulerOrder order);
 
 // copy operation for single node pose
-a3i32 a3spatialPoseCopy(a3_SpatialPose* spatialPose_out, const a3_SpatialPose* spatialPose_in);
+a3i32 a3spatialPoseCopy(a3_SpatialPose* spatialPose_out, a3_SpatialPose* spatialPose_in);
 
 //concatenate/combine
 a3i32 a3spatialPoseConcat(a3_SpatialPose* spatialPose_out, const a3_SpatialPose* spatialPose_lh,const a3_SpatialPose* spatialPose_rh);
