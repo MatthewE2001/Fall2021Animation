b44b0d8fcd6e58b43a1fdc2e7f8d4cf0ed51a67b
Matt <matthew.esslie@mymail.champlain.edu>
2021-12-02 09:07:29 -0500

    Adding some information to hierarchy script

--- Animation Final Project/Assets/Scripts/Hierarchy.cs
+++ Animation Final Project/Assets/Scripts/Hierarchy.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Hierarchy : MonoBehaviour
 {
+    int index;
+    int parentIndex;
+
+    //Node node;
+
     // Start is called before the first frame update
     void Start()
     {
