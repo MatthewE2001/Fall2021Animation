064857c93310b05307671e3902e72aeeb993766d
Matt <matthew.esslie@mymail.champlain.edu>
2021-12-03 09:35:34 -0500

    Setting up some of a keyframe script

---
+++ Animation Final Project/Assets/Scripts/Keyframe.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyframe : MonoBehaviour
+{
+    int index;
+    int durationStep;
+    int sampleIndex0, sampleIndex1;
+    float durationSeconds;
+    float durationInverse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitializeKeyframe(int tmpSample0, int tmpSample1, int timeStep, float playbackPerSec)
+    {
+        sampleIndex0 = tmpSample0;
+        sampleIndex1 = tmpSample1;
+        durationStep = timeStep;
+        durationSeconds = playbackPerSec;
+    }
+}
