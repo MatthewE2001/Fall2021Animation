32206d2caa8316b02cd010f36eb205529729fb05
Matt <matthew.esslie@mymail.champlain.edu>
2021-12-03 09:48:25 -0500

    Starting a clip script

---
+++ Animation Final Project/Assets/Scripts/Clip.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clip : MonoBehaviour
+{
+    string name;
+    int index;
+    int firstKeyframe, finalKeyframe, keyframeCount; //do 1st and final frames need to be keyframe?
+    bool keyframeDirection; //this could also be an integer set to 1 or 0
+    int durationStep;
+    float durationSec, durationInverse;
+
+    //clip transitions?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitializeClip(string tmpName, Keyframe keyframeFirst, Keyframe keyframeFinal)
+    {
+        name = tmpName;
+        //firstKeyframe = keyframeFirst.GetComponent<Keyframe>().GetIndex();
+        //finalKeyframe = keyframeFinal.GetComponent<Keyframe>().GetIndex();
+    }
+}
