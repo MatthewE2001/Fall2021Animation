c0aeddb16e43dcf7c9a64d30bbc0f70609dc35f3
bdavies18 <48997025+bdavies18@users.noreply.github.com>
2021-12-12 02:44:58 -0500

    Playback system updates, including playback system controls.

--- Animation Final Project/Assets/Scripts/UserInterface/Playback.cs
+++ Animation Final Project/Assets/Scripts/UserInterface/Playback.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Playback : MonoBehaviour
 {
+
+    public Button playButton;
+    public Button pauseButton;
+
     bool isPaused;
+    bool isPlaying;
 
     // Start is called before the first frame update
     void Start()
@@ -21,28 +27,91 @@ public class Playback : MonoBehaviour
 
     public void onPlay()
     {
-        if (GetComponent<AudioSource>().clip != null)
+        if (isPlaying)
         {
+            // Stop playing
             isPaused = false;
-            GetComponent<AudioSource>().Play();
+            isPlaying = false;
+
+            // Set the play button to inactive (Gray)
+            playButton.GetComponent<Image>().color = new Color(88f / 255f, 88f / 255f, 88f / 255f);
+            // Set the pause button to inactive (Gray 88)
+            pauseButton.GetComponent<Image>().color = new Color(88f / 255f, 88f / 255f, 88f / 255f);
+
+            // Stop playing audio and reset
+            if (GetComponent<AudioSource>().clip != null)
+            {
+                GetComponent<AudioSource>().Stop();
+            }
+            else
+            {
+                Debug.Log("No audio clip loaded!");
+            }
+
+            // Stop playing animation and reset
         }
         else
         {
-            Debug.Log("No clip loaded!");
+            // Start playing
+            isPaused = false;
+            isPlaying = true;
+
+            // Set the play button to active (Blue)
+            playButton.GetComponent<Image>().color = new Color(35f / 255f, 74f / 255f, 108f / 255f);
+            // Set the pause button to inactive (Gray 88)
+            pauseButton.GetComponent<Image>().color = new Color(88f / 255f, 88f / 255f, 88f / 255f);
+
+            // Start playing audio from beginning
+            if (GetComponent<AudioSource>().clip != null)
+            {
+                GetComponent<AudioSource>().Play();
+            }
+            else
+            {
+                Debug.Log("No audio clip loaded!");
+            }
+
+            // Start playing animation from beginning
         }
+
+
     }
 
     public void onPause()
     {
-        if (isPaused)
+        // Pause button only works in play state
+        if (isPlaying)
         {
-            isPaused = false;
-            GetComponent<AudioSource>().UnPause();
+            if (isPaused)
+            {
+                // Stop pause state
+                isPaused = false;
+
+                // Set the pause button to inactive (Gray 88)
+                pauseButton.GetComponent<Image>().color = new Color(88f / 255f, 88f / 255f, 88f / 255f);
+
+                // Unpause audio
+                GetComponent<AudioSource>().UnPause();
+
+                // Unpause animation
+            }
+            else
+            {
+                // Start pause state
+                isPaused = true;
+
+                // Set the pause button to active (Gray 106)
+                pauseButton.GetComponent<Image>().color = new Color(106f / 255f, 106f / 255f, 106f / 255f);
+
+                // Pause audio
+                GetComponent<AudioSource>().Pause();
+
+                // Pause animation
+            }
         }
         else
         {
-            isPaused = true;
-            GetComponent<AudioSource>().Pause();
+            Debug.Log("Not in play state!");
         }
 
     }
